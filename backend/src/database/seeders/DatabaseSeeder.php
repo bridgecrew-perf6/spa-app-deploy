be8d36421c134f8f0d4f7cacbf97f8cba149fb0e
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-02-15 00:22:42 +0900

    db seeder導入

--- backend/src/database/seeders/DatabaseSeeder.php
+++ backend/src/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create([
+            'name' => 'システム管理者アカウント',
+            'email' => 'sys_admin@qlife.co.jp',
+            'password' => Hash::make('admin1234'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        \App\Models\User::factory(10)->create();
     }
 }
