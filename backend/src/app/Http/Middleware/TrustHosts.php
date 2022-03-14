92f8c988609f320f7ff6c152064e5ac4cc14362c
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-03-14 10:58:43 +0900

    laravel https対応

--- backend/src/app/Http/Middleware/TrustHosts.php
+++ backend/src/app/Http/Middleware/TrustHosts.php
@@ -6,15 +6,19 @@ use Illuminate\Http\Middleware\TrustHosts as Middleware;
 
 class TrustHosts extends Middleware
 {
+    protected $proxies = '*';
+
+    protected $headers = Request::HEADER_X_FORWARDED_AWS_ELB;
+
     /**
      * Get the host patterns that should be trusted.
      *
      * @return array<int, string|null>
      */
-    public function hosts()
-    {
-        return [
-            $this->allSubdomainsOfApplicationUrl(),
-        ];
-    }
+    // public function hosts()
+    // {
+    //     return [
+    //         $this->allSubdomainsOfApplicationUrl(),
+    //     ];
+    // }
 }
